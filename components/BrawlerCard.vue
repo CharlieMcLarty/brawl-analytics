201d5fb24fb320d3812da06238a9d83619caac6c
Charlie McLarty <89890432+CharlieMcLarty@users.noreply.github.com>
2025-02-19 14:04:44 -0500

    styled brawler cards

--- components/BrawlerCard.vue
+++ components/BrawlerCard.vue
@@ -5,16 +5,43 @@
         trophies: number;
         power: number;
     }
-
     const props = defineProps<Brawler>();
-
     const getBrawlerPotrait = (id: number) => {
         return `https://cdn.brawlify.com/brawlers/borderless/${id}.png`;
     }
 </script>
 
 <template>
-    <div class="brawler-card">
-        <img :src="getBrawlerPotrait(props.id)" alt="brawler" />
+    <div class="brawler-card w-64">
+        <div class="border-2 border-black relative">
+            <div class="absolute top-2 left-2 w-10 h-10 bg-gray-800 rounded-full flex items-center justify-center border-2 border-yellow-400">
+                <span class="text-white text-sm font-bold">{{ props.power }}</span>
+            </div>
+            <img class="w-64 h-64 object-cover" 
+                :src="getBrawlerPotrait(props.id)" 
+                alt="brawler" 
+            />
+        </div>
+
+        <div class="border-t-1 border-black p-1">
+            <div class="flex justify-between items-center gap-8">
+                <span>{{ props.name }}</span>
+                <div class="flex items-center gap-1">
+                    <span class="text-yellow-500">🏆</span>
+                    <span>{{ props.trophies }}</span>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
+
+<style scoped>
+.brawler-card {
+    display: inline-block;
+    transition: transform 0.2s;
+}
+
+.brawler-card:hover {
+    transform: scale(1.05);
+}
+</style>
--- pages/player/[tag].vue
+++ pages/player/[tag].vue
@@ -1,6 +1,4 @@
 <script setup>
-import BrawlerCard from '~/components/BrawlerCard.vue';
-
 const route = useRoute()
 const { data: playerData, status, error } = await useFetch('/api/player', {
   query: { tag: route.params.tag }
@@ -8,14 +6,14 @@ const { data: playerData, status, error } = await useFetch('/api/player', {
 </script>
 
 <template>
-    <div>
+    <div class="container mx-auto p-4">
         <div v-if="status === 'pending'">Loading...</div>
             <div v-else-if="error">{{ error.message }}</div>
-            <div v-else-if="playerData">
-                <h1>{{ playerData['name'] }}</h1>
+            <div v-else-if="playerData" class="mb-8">
+                <h1 class="text-2xl font-bold mb-2">{{ playerData.name }}</h1>
                 <p>Trophies: {{playerData.trophies }}</p>
             </div>
-            <div class="brawler-grid">
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
                 <BrawlerCard v-for="brawler in playerData.brawlers" 
                     :id="brawler.id" 
                     :name="brawler.name" 
